89301ec0e39aa4e57418c07412cadcd03fc2174b
Ian Oliva <yoian@outlook.com>
2022-03-25 21:24:14 -0600

    Trying "Try" and "Except" on debugging.py

--- debugging.py
+++ debugging.py
@@ -10,14 +10,17 @@
 def divisors(num):
     divisors = []
     for i in range(1, num + 1):
-        if num % i == 1:
+        if num % i == 0:
             divisors.append(i)
     return divisors
 
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Terminó mi programa")
+    try:
+        num = int(input("Ingresa un número: "))
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Solo se pueden ingresar números")
 
 
 if __name__ == "__main__":
